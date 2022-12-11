7a11b14f4b44573a1822ea8549f732b598ca0b38
wraith-0 <navabhi.dev@gmail.com>
2022-12-11 09:43:20 +0530

    added models and embeddings

---
+++ webscrapping/README.md
@@ -0,0 +1,4 @@
+# BTP
+
+- `scrapAppSafetyData.js` - This will scrap the play store for the satefy data of ~2000 apps, distributed across 50 categories and generate `safetyData.json`
+- `embedding.js` - This file will generate the embeddings for data in `safetyData.json`, one can choose from three different techniques.
--- webscrapping/embedding.js
+++ webscrapping/embedding.js
@@ -6,28 +6,51 @@ import safetyData from "./safetyDataPreety.js";
 
 let res = "";
 
+// ******* Embedding 1 *******
+
+// for (var i = 0; i < safetyData.length; i++) {
+//   let temp = safetyData[i].companyCategory;
+//   // Data Shared
+//   for (var j = 0; j < fields.length; j++) {
+//     let len = 0;
+//     if (safetyData[i].dataShared[fields[j]] != undefined) {
+//       len = safetyData[i].dataShared[fields[j]].length;
+//     }
+//     if (len > 0) temp += "," + 1;
+//     else temp += "," + 0;
+//   }
+//   // Data Collected
+//   for (var j = 0; j < fields.length; j++) {
+//     let len = 0;
+//     if (safetyData[i].dataCollected[fields[j]] != undefined) {
+//       len = safetyData[i].dataCollected[fields[j]].length;
+//     }
+//     if (len > 0) temp += "," + 1;
+//     else temp += "," + 0;
+//   }
+//   res += temp;
+//   res += "\n";
+// }
+
 // ******* Embedding 2 *******
 
 // for (var i = 0; i < safetyData.length; i++) {
 //   let temp = safetyData[i].companyCategory;
+//   // Data Shared
 //   for (var j = 0; j < fields.length; j++) {
-//     if (
-//       fields[j] == "Data is encrypted in transit" ||
-//       fields[j] == "Data can’t be deleted" ||
-//       fields[j] == "You can request that data be deleted" ||
-//       fields[j] == "Independent security review"
-//     ) {
-//       if (safetyData[i][fields[j]].length == 1) {
-//         temp += ",1";
-//       } else temp += ",0";
-//     } else {
-//       for (var k = 0; k < purpose.length; k++) {
-//         //console.log(safetyData[i][fields[j]].includes(purpose[k]));
-//         if (safetyData[i][fields[j]].includes(purpose[k])) {
-//           temp += ",1";
-//         } else temp += ",0";
-//       }
+//     let len = 0;
+//     if (safetyData[i].dataShared[fields[j]] != undefined) {
+//       len = safetyData[i].dataShared[fields[j]].length;
 //     }
+//     temp += "," + len;
+//   }
+//   // Data Collected
+//   for (var j = 0; j < fields.length; j++) {
+//     let len = 0;
+//     if (safetyData[i].dataCollected[fields[j]] != undefined) {
+//       len = safetyData[i].dataCollected[fields[j]].length;
+//     }
+//     temp += "," + len;
 //   }
 //   res += temp;
 //   res += "\n";
@@ -43,6 +66,11 @@ for (var i = 0; i < safetyData.length; i++) {
     if (safetyData[i].dataShared[fields[j]] != undefined) {
       len = safetyData[i].dataShared[fields[j]].length;
     }
+    if (fields[j].includes("Optional")) {
+      len *= 5;
+    } else {
+      len *= 10;
+    }
     temp += "," + len;
   }
   // Data Collected
@@ -50,6 +78,12 @@ for (var i = 0; i < safetyData.length; i++) {
     let len = 0;
     if (safetyData[i].dataCollected[fields[j]] != undefined) {
       len = safetyData[i].dataCollected[fields[j]].length;
+      if (fields[j].includes("Optional")) {
+        len *= 5;
+      } else {
+        len *= 10;
+      }
+      temp += "," + len;
     }
     temp += "," + len;
   }
@@ -57,41 +91,11 @@ for (var i = 0; i < safetyData.length; i++) {
   res += "\n";
 }
 
-// ******* Embedding 4 *******
-
-// for (var i = 0; i < safetyData.length; i++) {
-//   let temp = safetyData[i].companyCategory;
-//   for (var j = 0; j < fields.length; j++) {
-//     if (
-//       fields[j] == "Data is encrypted in transit" ||
-//       fields[j] == "Data can’t be deleted" ||
-//       fields[j] == "You can request that data be deleted" ||
-//       fields[j] == "Independent security review"
-//     ) {
-//       if (safetyData[i][fields[j]].length == 1) {
-//         temp += ",1";
-//       } else temp += ",0";
-//     } else {
-//       var len = safetyData[i][fields[j]].length;
-//       if (fields[j].includes("Optional") || fields[j].includes("(Optional)")) {
-//         len *= 5;
-//       } else {
-//         len *= 10;
-//       }
-//       temp += "," + len;
-//     }
-//   }
-//   res += temp;
-//   res += "\n";
-// }
-
 // ************ WRITE TO FILE ************
 
-fs.writeFile("./embedding.csv", res, function (err) {
+fs.writeFile("./embedding3.csv", res, function (err) {
   if (err) console.log(err);
   else {
     console.log("Done");
   }
 });
-
-//console.log(data);
